1f994c7468cefe70bc0154361087792046ce93d6
UarabeyB <uarabeyb@gmail.com>
2014-09-27 11:50:29 +0300

    удалил лишнюю вью

--- RuSpeak/Controllers/PostController.cs
+++ RuSpeak/Controllers/PostController.cs
@@ -74,34 +74,6 @@ namespace RuSpeak.Controllers
             }
         }
 
-        [HttpGet]
-        public ActionResult UploadAudio()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult UploadAudio(HttpPostedFileBase file) 
-        {
-            if (file != null && file.ContentLength > 0)
-                try
-                {
-                    string path = Path.Combine(Server.MapPath("~/Images"),
-                                               Path.GetFileName(file.FileName));
-                    file.SaveAs(path);
-                    ViewBag.Message = "File uploaded successfully";
-                }
-                catch (Exception ex)
-                {
-                    ViewBag.Message = "ERROR:" + ex.Message.ToString();
-                }
-            else
-            {
-                ViewBag.Message = "You have not specified a file.";
-            }
-            return View();
-        }
-
         public ActionResult Delete(int id)
         {
             postRepository.DeletePost(id);
